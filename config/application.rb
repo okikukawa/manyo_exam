e98ddc371575c9ccf39ed388858a2a088bb3863c
okikukawa <kikukawa0131@gmail.com>
2020-11-16 14:59:56 +0900

    タイムゾーンを東京に指定

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,8 @@ module ManyoExam
                    controller_specs: false,
                    request_specs: false
       end
+      config.time_zone = 'Tokyo'
+      config.active_record.default_timezone = :local
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
