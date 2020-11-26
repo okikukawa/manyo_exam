0ee4a26ece1b9d69a11b98691f8ca90d2f1a84e6
okikukawa <kikukawa0131@gmail.com>
2020-11-26 15:03:40 +0900

    管理者アカウントが0にならないようcallbackを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,14 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length:{ minimum:6 }
 
-  # before_destroy :check_admin_count
-  # before_update :check_admin_count
+  before_destroy :check_admin_count
+  before_update :check_admin_count
   has_many :tasks, dependent: :destroy
 
-  # def check_admin_count
-  #   @admin_count = User.where(admin: true).count
-  #   if @admin_count <= 1
-  #     throw :abort, notice:"少なくとも1ユーザーは管理者権限を有する必要があります。"
-  #   end
-  # end
+  def check_admin_count
+    @admin_count = User.where(admin: true).count
+    if @admin_count <= 2
+      throw :abort
+    end
+  end
 end
