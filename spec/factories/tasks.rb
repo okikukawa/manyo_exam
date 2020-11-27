94bc88af997936a6db24c4b12a7bf84e2c7c79df
okikukawa <kikukawa0131@gmail.com>
2020-11-27 11:38:19 +0900

    gitignoreからpublic/assetsの記述を削除

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -5,22 +5,12 @@ FactoryBot.define do
     deadline { '002020-11-25' }
     status { '未着手' }
     priority { '高' }
-    user
   end
   factory :second_task, class: Task do
     title { 'Factoryで作ったデフォルトのタイトル２' }
     content { 'Factoryで作ったデフォルトのコンテント２' }
     deadline { '002020-11-30' }
     status { '完了' }
-    priority { '低' }
-    association :user
-  end
-  factory :third_task, class: Task do
-    title { 'Factoryで作ったデフォルトのタイトル３' }
-    content { 'Factoryで作ったデフォルトのコンテント３' }
-    deadline { '002020-12-25' }
-    status { '未着手' }
     priority { '中' }
-    association :user
   end
 end
